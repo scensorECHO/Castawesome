e94caf66259adc1291d96bcbb11ac1c181013a15
scensorECHO <xxartofwarxx@gmail.com>
2015-11-13 22:23:24 -0500

    Added pulseaudio command to get alsa device monitors

--- custom_ffmpeg.sh
+++ custom_ffmpeg.sh
@@ -3,15 +3,16 @@ OUTRES="1280x720" # output resolution
 FPS="24" # target FPS
 GOP="48" # i-frame interval, should be double of FPS,
 GOPMIN="24" # min i-frame interval, should be equal to fps,
-THREADS="2" # max 6
+THREADS="1" # max 6
 CBR="1500k" # constant bitrate (should be between 1000k - 3000k)
 QUALITY="fast"  # one of the many FFMPEG preset # ultrafast
 AUDIO_RATE="44100" # two other options for aac audio codecs
-STREAM_KEY="" # retrieve from the stream_key file
+STREAM_KEY="$1" # retrieve from the stream_key file
 SERVER="live-jfk" # twitch server in new york, see http://bashtech.net/twitch/ingest.php for list
 ffmpeg \
   -f alsa -i pulse \ # smart detects output stream and can be selected from pavucontrol after launch
-  -f pulse -i "alsa_device" \ #alsa_output.usb-Skullcandy_Skullcandy_Slayer_000000000000-00.iec958-stereo.monitor
+  -f pulse -i "alsa_device" \ # pactl list, and retrieve the monitor source
+	 #alsa_output.usb-Skullcandy_Skullcandy_Slayer_000000000000-00.iec958-stereo.monitor
   -f x11grab -s "$INRES" -r "$FPS" -i :0.0+0,1080 \ # pulls slected area in for stream, current for dual vertical monitors
   -filter_complex amerge -ac 2 -ar $AUDIO_RATE -c:a aac \ # merge audio streams in flv compatible codec (aac)
   -f flv \ # output to flv format
